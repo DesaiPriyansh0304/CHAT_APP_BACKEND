0be80b146e9713518d2776b948da8b4c67e97428
kananimaulik26@gmail.com <desaipri0304@gmail.com>
2025-07-30 15:06:38 +0530

    deplpoy backend

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-fronted-lovat.vercel.app",
+    ],
     credentials: true,
   })
 );
